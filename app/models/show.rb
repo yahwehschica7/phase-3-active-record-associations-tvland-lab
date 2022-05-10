2dda0e91ac3dfa06034089e99ee58ceef50caaaf
Jessica Nelson <yahwehschica@yahoo.com>
2022-05-10 09:52:01 -0400

    commit tree 2a24e8b5881d7d4621ba371aac045556bd5d3b60

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
   
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network 
+
+def actors_list 
+    self.actors.map do |actor|
+    "#{actor.first_name} #{actor.last_name}"
+    end
+end
+  
 end
